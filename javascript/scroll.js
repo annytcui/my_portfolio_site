d96d2439215758119947cb1159d9ae117238f4e1
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-07-12 17:15:05 +0800

    Edit styles

--- javascript/scroll.js
+++ javascript/scroll.js
@@ -3,6 +3,7 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollPos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").classList.add("nav-up");
   } else {
     document.getElementById("navbar").style.top = "-50px";
   }
